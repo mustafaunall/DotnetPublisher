fe81ce4ba31535f7761aad396b35153947e67987
Mustafa Ünal <revengefiretr@gmail.com>
2022-05-17 17:05:01 +0300

    initialize project files & main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,30 @@
-console.log('Hello World')
+import { readFileSync } from 'fs'
+import { Client } from 'ssh2'
+import { config } from 'dotenv'
+config()
+
+const { host, port, username, password } = process.env
+
+const conn = new Client()
+
+conn.connect({
+    host, port: Number(port), username, password
+})
+
+conn.on('ready', () => {
+    console.log('Client is ready')
+    conn.exec('cd $HOME', (err, stream) => {
+        if (err) throw err
+        stream.on('data', (data: any) => {
+            console.log(`Output is :: ${data}`)
+
+            conn.exec('~/santral_publish', (err, strPub) => {
+                if (err) throw err
+                strPub.on('data', (dataPub: any) => {
+                    console.log(`Output is :: ${dataPub}`)
+                })
+            })
+        })
+    })
+})
+
